8c7da25e7a5eb7173c0b290fe409ff95ff7c0945
root <root@localhost.localdomain>
2019-02-12 22:34:30 +0530

    The Menu driven program for different commands

--- python1.py
+++ python1.py
@@ -2,7 +2,6 @@
 import os
 import sys
 import webbrowser
-<<<<<<< HEAD
 
 n=1
 while(n!=0):
@@ -25,21 +24,3 @@ while(n!=0):
 		n=0	
 	else:
 		print("Wrong Input")
-=======
-var=int(input("Press1:Date, Press2:Calender, Press3:Shutdown, Press4:For IP, Press5:ALL IP, Press6:Google Search"))
-if(var==1):
-	os.system('date')
-elif(var==2):
-	os.system('calender')
-elif(var==3):
-	os.system('shutdown -H -T -5')
-elif(var==4):
-	os.system('hostname -I')
-elif(var==5):
-	os.system('ifconfig')
-elif(var==6):
-	wb=input("Enter Keywords")
-	webbrowser.open_new_tab('https://www.google.com/search?q={}.format(wb)')
-else:
-	print("Wrong Input")
->>>>>>> 44986d264aab2298dfc7fcb5b588913453033b88
