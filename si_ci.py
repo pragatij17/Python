7ea10f3fc23e593dcad79d84f958eeaf5c03bbdd
pragatij17 <pj.pragatijain09@gmail.com>
2021-05-19 20:15:51 +0530

    Added changes in CI Formula

--- si_ci.py
+++ si_ci.py
@@ -1,13 +1,14 @@
 def simple_interest(p, r, t): 
     si = (p * r * t)/100
     return si
-def compound_interest(p, r, t):
-    amount = p*(pow((1+r/100),t))
+def compound_interest(p, r, t, n):
+    amount = p*(pow((1+r/n),(n*t)))
     ci = amount-p
     return ci
 
 p=int(input('The principal is: '))
 t=float(input('The time period is: '))
-r=int(input('The rate of interest is: '))
+r=float(input('The rate of interest is: '))
+n=int(input('Number of times: '))
 print(simple_interest(p, r, t))
-print(compound_interest(p, r, t))
+print(compound_interest(p, r, t, n))
